86c601c95a0c092355641ea28516ea9eaa095d71
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-10 06:36:27 +0000

    Conversion of Datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+"use strict"
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"-->33
+//"33abc"-->NaN
+//true-->1 false->0
+//NULL->0
+//undefined-->NaN
+
+// let isLoggedIn="saurabh"
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+//1-->true 0-->false
+//""-->false
+//"saurabh"->true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
+
