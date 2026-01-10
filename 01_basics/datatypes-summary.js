1133994e828b217c56aa078f66f80b51609da7e6
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-10 10:14:02 +0000

    STACK AND HEAP

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -4,24 +4,48 @@
 //Reference Type(Non-primitive)
 //Array,Objects,Function
 
-const id=Symbol('123')
-const anotherId=Symbol('123')
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
 
-console.log(id);
-console.log(anotherId);
-console.log(id===anotherId);
+// console.log(id);
+// console.log(anotherId);
+// console.log(id===anotherId);
 
 
-const bigNumber=12555615651611211111111n
-console.log(typeof bigNumber);
+// const bigNumber=12555615651611211111111n
+// console.log(typeof bigNumber);
 
-const heros=["shaktiman","nagraaj","doga"];
-let myObj={
-    name:"saurabh",
-    age:22,
+// const heros=["shaktiman","nagraaj","doga"];
+// let myObj={
+//     name:"saurabh",
+//     age:22,
+// }
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+// console.log(typeof heros);
+
+
+
+///******************/
+
+//Stack(Primitive) ,Heap(Non-Primitive)
+
+let myYoutubename="saurabh"
+let anothername=myYoutubename
+anothername="yadav"
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne={
+    email:"saurabh@gmail.com",
+    upi:"user@ybl"
 }
-const myFunction = function(){
-    console.log("Hello World");
-}
+let userTwo=userOne
 
-console.log(typeof heros);
+userTwo.email="yadav@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
