4414f581a2e92e957a621834b5cf463ce2e02d29
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-10 06:16:26 +0000

    data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //  we are using nodejs not browser
+console.log(3+3); console.log("saurabh")
+
+let name="saurabh"
+let age=18;
+let isLoggedIn=false;
+
+// datatypes
+// number=> 2 to power 53
+// big int
+// string=""
+//boolean =>true/false
+//null------standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined--beacuse its a type
+console.log(typeof null) //object
+
