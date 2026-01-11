d8eaad21b382a9adbe1e6d6e115510d12472a47f
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-11 05:17:08 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,29 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(score.toString().length);
+// console.log(typeof score);
+// console.log(balance.toFixed(1));
+
+// const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString());
+
+// console.log(Math)// run this in browser
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(-4.4));
+// console.log(Math.floor(-4.4));
+// console.log(Math.min(4,6,8,1.2));
+// console.log(Math.max(4,6,8,1.2));
+//console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
