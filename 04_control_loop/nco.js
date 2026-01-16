c33b757fdbdf7fd87bf132905c8b46160281847f
saurabhrksss <saurabhrksss2@gmail.com>
2026-01-16 13:37:05 +0530

    NULLISH COALESCING OPERATOR

---
+++ 04_control_loop/nco.js
@@ -0,0 +1,14 @@
+// NULLISH COALESCING OPERATOR(??):NULL & UNDEFINED
+
+let val1;
+// val1=5 ?? 10
+// val1= null ?? 10
+// val1= undefined?? 10
+val1=null??10??20
+console.log(val1);
+
+//Terniary operator
+
+const iceteaprice=100
+iceteaprice>=80?console.log("More than"): console.log("less than");
+
